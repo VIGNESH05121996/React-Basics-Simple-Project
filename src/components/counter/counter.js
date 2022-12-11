1becb21c30d9fef980e744481400fb2bf3e010ea
Vignesh M <vignesh.marimuthu@emids.com>
2022-12-11 19:04:43 +0530

    counter component - created simple counter app

---
+++ src/components/counter/counter.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import './counter.css'
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-button" onClick={this.increment}>
+          +
+        </button>
+        <button className="counter-button" onClick={this.decrement}>
+          -
+        </button>
+        <div className="output">{this.state.count}</div>
+      </div>
+    );
+  }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import HelloWorld from './components/hello-world/helloWorld';
+import Counter from './components/counter/counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelloWorld />
+    {/* <HelloWorld /> */}
+    <Counter />
   </React.StrictMode>
 );
