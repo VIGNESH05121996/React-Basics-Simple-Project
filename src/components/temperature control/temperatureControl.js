679646c46501e7bdd7cecd1448da7b4b6ed02db9
Vignesh M <vignesh.marimuthu@emids.com>
2022-12-11 20:19:09 +0530

    temperature control component - created temperature control app

---
+++ src/components/temperature control/temperatureControl.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./temperatureControl.css";
+
+const TemperatureControl = () => {
+  const [temperatureValue, setTemperatureValue] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState("cold");
+
+  const increaseTemperature = () => {
+    const newTemperatureValue = temperatureValue + 1;
+    setTemperatureValue(newTemperatureValue);
+    if (newTemperatureValue >= 15) {
+      setTemperatureColor("hot");
+    }
+  };
+
+  const decreaseTemperature = () => {
+    const newTemperatureValue = temperatureValue - 1;
+    setTemperatureValue(newTemperatureValue);
+    if (newTemperatureValue < 15) {
+      setTemperatureColor("cold");
+    }
+  };
+
+  return (
+    <div className="whole-container">
+      <div className="temperature-container">
+        <div className={`display-degree ${temperatureColor}`}>
+          {temperatureValue}°C
+        </div>
+        <div className="button-container">
+          <button onClick={() => increaseTemperature()}>+</button>
+          <button onClick={() => decreaseTemperature()}>-</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TemperatureControl;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import HelloWorld from './components/hello-world/helloWorld';
 import Counter from './components/counter/counter';
+import TemperatureControl from './components/temperature control/temperatureControl';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <HelloWorld /> */}
-    <Counter />
+    {/* <HelloWorld />
+    <Counter /> */}
+    <TemperatureControl />
   </React.StrictMode>
 );
